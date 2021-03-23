0387d97e5367ae3074c03739d153164e2338f9cb
qjzl2008 <qjzl2008@163.com>
2021-03-23 14:50:55 +0800

    Delete elasticsearch directory

--- elasticsearch/logsys/api/python-ext/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python setup.py build
-cp build/*/*.so .
-cp build/*/*.so ../lib
--- elasticsearch/logsys/api/python-ext/demo.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-import log_py
-log_py.nglog_set_option_str("ServiceName", "TestPySvc.TestPySvc")
-log_py.nglog_error('somthing error.')
